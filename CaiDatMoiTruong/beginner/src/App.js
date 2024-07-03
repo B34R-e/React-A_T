35319f50236d7b655c83d25719b142acc97183c5
Hoàng Tuấn Minh <wheycoffee1607@gmail.com>
2024-07-03 14:22:45 +0700

    first update

---
+++ CaiDatMoiTruong/beginner/src/App.js
@@ -0,0 +1,9 @@
+import logo from './logo.svg'
+import './App.css'
+import BareInput from './BareInput'
+
+function App() {
+  return <BareInput />
+}
+
+export default App
---
+++ CaiDatMoiTruong/beginner/src/BareInput.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+// class BareInput extends React.Component {
+//   render() {
+//     return <input {...this.props} />
+//   }
+// }
+
+function BareInput(props) {
+  return (
+    <div>
+      <input {...this.props} />
+    </div>
+  )
+}
+
+export default BareInput
